78ceb885ced11837a20beb372b1f8b67f69252e4
Tarkan Kara <unnahmet23@hotmail.com>
2022-11-05 23:58:39 +0300

    Users Info

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_local_variable
+// ignore_for_file: unused_local_variable, unused_element
 
 import 'package:flutter/material.dart';
 
@@ -30,14 +30,72 @@ class ProfileScreen extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Text(
-          "@@${userss.username}",
+          "@@${userss.username} - ${userss.likes} likes",
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
                 color: Colors.red,
                 fontSize: 25,
               ),
         ),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [_ProfileInformation(users: userss)],
+        ),
+      ),
+    );
+  }
+}
+
+class _ProfileInformation extends StatelessWidget {
+  final User users;
+  const _ProfileInformation({required this.users});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.white,
+          backgroundImage: AssetImage(users.imagePath),
+        ),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Row(
+            children: [
+              _buildUserInfo(context, "Following", "${users.followings}"),
+              _buildUserInfo(context, "Folowers", "${users.followers}"),
+              _buildUserInfo(context, "Likes", "${users.likes}"),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Expanded _buildUserInfo(BuildContext context, String type, String value) {
+    return Expanded(
+      child: Column(
+        children: [
+          Text(
+            value,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            type,
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                  letterSpacing: 1.5,
+                ),
+          ),
+        ],
+      ),
     );
   }
 }
