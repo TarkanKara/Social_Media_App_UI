1f63153f2b96d2693c14ea694cd241fc91b4c4ef
Tarkan Kara <unnahmet23@hotmail.com>
2022-11-05 23:37:01 +0300

    send user parameter{User}

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
+
+import '../models/post_model.dart';
+import '../models/user_model.dart';
 
 class ProfileScreen extends StatelessWidget {
   //page routeName
@@ -7,6 +12,32 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    //
+    //Sadece kullanıcı parametresini alıp yönlendirme işlemleri
+    User? userss = ModalRoute.of(context)!.settings.arguments as User?;
+    userss = userss ??= User.users[0];
+
+    List<Post> ptss = Post.posts.where(
+      (element) {
+        return element.user.id == userss!.id;
+      },
+    ).toList();
+    //
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "@@${userss.username}",
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Colors.red,
+                fontSize: 25,
+              ),
+        ),
+      ),
+      body: Container(),
+    );
   }
 }
