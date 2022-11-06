571579fbdc982d77d594cf316d321a2b0a54bb23
Tarkan Kara <unnahmet23@hotmail.com>
2022-11-06 18:06:03 +0300

    packages : flutter_staggered_grid_view ||  MasonryGridView

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
+import 'package:social_media_app/models/user_model.dart';
+import 'package:social_media_app/screen/profile_screen.dart';
 import 'package:social_media_app/widgets/custom_bottom_app_bar.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SearchScreen extends StatelessWidget {
   //page routeName
@@ -8,6 +13,7 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<User> users = User.users;
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -24,6 +30,93 @@ class SearchScreen extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: const CustomBottomAppBar(),
+      body: MasonryGridView.count(
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        padding: const EdgeInsets.all(10),
+        shrinkWrap: true,
+        itemCount: users.length,
+        crossAxisCount: 2,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                ProfileScreen.routeName,
+                arguments: users[index],
+              );
+            },
+            child: Stack(
+              children: [
+                Container(
+                  height: (index == 0) ? 250 : 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(users[index].imagePath),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const Positioned.fill(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.black,
+                        ],
+                        begin: Alignment.center,
+                        end: Alignment.bottomCenter,
+                        stops: [0.4, 1.0],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 10,
+                  bottom: 10,
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage(users[index].imagePath),
+                      ),
+                      const SizedBox(width: 10),
+                      const SizedBox(height: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            users[index].username,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            "2 min ago",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
