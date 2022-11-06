0be1aa611169e8c338d47d5609cfae0a01ecc674
Tarkan Kara <unnahmet23@hotmail.com>
2022-11-06 17:15:36 +0300

    created search_screen.dart

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/widgets/custom_bottom_app_bar.dart';
 
 class SearchScreen extends StatelessWidget {
   //page routeName
@@ -7,6 +8,22 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        automaticallyImplyLeading: false, // Remove back button
+        centerTitle: true,
+        title: Text(
+          "Discover",
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Colors.white),
+        ),
+      ),
+      bottomNavigationBar: const CustomBottomAppBar(),
+    );
   }
 }
